68f4730ec1072c19b23a61a1653a305100ac09fa
Aaron Martin <grav3serker@Aarons-MacBook-Pro.local>
2016-04-22 00:02:28 -0700

    updated code

--- mirror.js
+++ mirror.js
@@ -1,19 +1,19 @@
-function mirror (word) {
-  var mirroredWord
-
-  if (word) {
-    mirroredWord = word + '|'
-
-    var lastCharPosition = word.length - 1
-    while (lastCharPosition >= 0) {
-      mirroredWord += word[lastCharPosition]
-      lastCharPosition--
-    }
-  }
-
-  return mirroredWord
-}
-
-var pickWord = prompt('Enter a word!')
-var mirroredWord = mirror(pickWord)
-console.log(mirroredWord)
+// function mirror (word) {
+//   var mirroredWord
+//
+//   if (word) {
+//     mirroredWord = word + '|'
+//
+//     var lastCharPosition = word.length - 1
+//     while (lastCharPosition >= 0) {
+//       mirroredWord += word[lastCharPosition]
+//       lastCharPosition--
+//     }
+//   }
+//
+//   return mirroredWord
+// }
+//
+// var pickWord = prompt('Enter a word!')
+// var mirroredWord = mirror(pickWord)
+// console.log(mirroredWord)
--- sleepStats.js
+++ sleepStats.js
@@ -1,65 +1,65 @@
-// function getDaysFromYears (years) {
-//   var days
+function getDaysFromYears (years) {
+  var days
+
+  if (years) {
+    days = 365 * years
+  }
+
+  return days
+}
+
+function getSleepingHoursFromDays (days) {
+  var sleepingHours
+
+  if (days) {
+    sleepingHours = days * 8
+  }
+
+  return sleepingHours
+}
+
+
+function getDaysAsleep (sleepingHours) {
+  var daysSleeping
+
+  if (sleepingHours) {
+    daysSleeping = sleepingHours / 24
+  }
+
+  return daysSleeping
+}
+
+function getYearsAsleep (sleepingDays) {
+  var yearsSleeping
+
+  if (sleepingDays) {
+    yearsSleeping = sleepingDays / 365
+  }
+
+  return yearsSleeping
+}
+
+var yearsAlive = prompt('How old are you?')
+
+var daysIveBeenLiving = getDaysFromYears(yearsAlive)
+console.log('Days I have been alive: ', daysIveBeenLiving)
+
+var hoursIveBeenSleeping = getSleepingHoursFromDays(daysIveBeenLiving)
+console.log('Hours I have been sleeping: ', hoursIveBeenSleeping)
+
+var daysIveBeenAsleep = getDaysAsleep(hoursIveBeenSleeping)
+console.log('Days I have been asleep:', daysIveBeenAsleep)
+
+var yearsIveBeenAsleep = getYearsAsleep(daysIveBeenAsleep)
+console.log('Years I have been asleep:', yearsIveBeenAsleep)
+
+
+
+// function prompt (question) {
+//   var answer
 //
-//   if (years) {
-//     days = 365 * years
-//   }
+//   alert and get stuff
 //
-//   return days
+//   return answer
+//
 // }
-//
-// function getSleepingHoursFromDays (days) {
-//   var sleepingHours
-//
-//   if (days) {
-//     sleepingHours = days * 8
-//   }
-//
-//   return sleepingHours
-// }
-//
-//
-// function getDaysAsleep (sleepingHours) {
-//   var daysSleeping
-//
-//   if (sleepingHours) {
-//     daysSleeping = sleepingHours / 24
-//   }
-//
-//   return daysSleeping
-// }
-//
-// function getYearsAsleep (sleepingDays) {
-//   var yearsSleeping
-//
-//   if (sleepingDays) {
-//     yearsSleeping = sleepingDays / 365
-//   }
-//
-//   return yearsSleeping
-// }
-//
-// var yearsAlive = prompt('How old are you?')
-//
-// var daysIveBeenLiving = getDaysFromYears(yearsAlive)
-// console.log('Days I have been alive: ', daysIveBeenLiving)
-//
-// var hoursIveBeenSleeping = getSleepingHoursFromDays(daysIveBeenLiving)
-// console.log('Hours I have been sleeping: ', hoursIveBeenSleeping)
-//
-// var daysIveBeenAsleep = getDaysAsleep(hoursIveBeenSleeping)
-// console.log('Days I have been asleep:', daysIveBeenAsleep)
-//
-// var yearsIveBeenAsleep = getYearsAsleep(daysIveBeenAsleep)
-// console.log('Years I have been asleep:', yearsIveBeenAsleep)
-//
-//
-//
-// // function prompt (question) {
-// //   var answer
-// //
-// //   alert and get stuff
-// //
-// //   return answer
-// //
-// // }
