f81f08b6e0df73366ecddccac1afe2b84e4205d2
Aaron Martin <grav3serker@Aarons-iMac.local>
2016-04-23 21:29:59 -0700

    fixed code

--- rockpaperscissors.js
+++ rockpaperscissors.js
@@ -1,36 +1,49 @@
-var userChoice = prompt('Do you choose rock, paper, or scissors?')
-var computerChoice = Math.random()
-
-if (computerChoice <= 0.33 ) {
-    computerChoice = 'rock'
-} else if (computerChoice > 0.33 <= 0.66) {
-    computerChoice = 'paper'
+var userChoice = prompt("Do you choose rock, paper or scissors?");
+var computerChoice = Math.random();
+if (computerChoice < 0.34) {
+	computerChoice = "rock";
+} else if(computerChoice <= 0.67) {
+	computerChoice = "paper";
 } else {
-    computerChoice = 'scissors'
+	computerChoice = "scissors";
+} alert("Computer: " + computerChoice);
+
+function compare (choice1, choice2) {
+
+if(choice1 === choice2) {
+    alert("The result is a tie!");
 }
 
-alert('You chose ' + userChoice + ' and the computer chose ' + computerChoice)
+else if(choice1 === "rock") {
 
-// function compare (choice1, choice2) {
-//
-// if (userChoice === computerChoice) {
-//   alert("It's a tie")
-// } else if (userChoice = 'rock' && computerChoice = 'paper') {
-//   alert('The computer wins')
-// }
-//   else if (userChoice = 'rock' && computerChoice = 'scissors') {
-//     alert('You win!')
-// }
-//   else if (userChoice = 'paper' && computerChoice = 'rock') {
-//     alert('You win!')
-// }
-//   else if (userChoice = 'scissors' && computerChoice = 'rock') {
-//     alert('The computer wins')
-// }
-//   else if (userChoice = 'scissors' && computerChoice = 'paper') {
-//     alert('You win!')
-// }
-//   else if (userChoice = 'paper' && computerChoice = 'scissors') {
-//     alert('The computer wins')
-//   }
-// }
+    if(choice2 === "scissors") {
+        alert("rock wins");
+    }
+    else {
+        alert("paper wins");
+    }
+}
+
+else if(choice1 === "paper") {
+
+    if(choice2 === "rock") {
+        alert("paper wins");
+    }
+    else {
+        alert("scissors wins");
+    }
+}
+
+else if(choice1 === "scissors") {
+
+    if(choice2 === "rock") {
+        alert("rock wins");
+    }
+    else {
+        alert("scissors wins");
+    }
+}
+
+}
+
+compare(userChoice, computerChoice)
