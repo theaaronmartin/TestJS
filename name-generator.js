c267d444463ad421b2ee1221dc375f5ccf3b4f99
Aaron Martin <grav3serker@Aarons-iMac.local>
2016-04-23 21:59:01 -0700

    added name-generator.js

---
+++ name-generator.js
@@ -0,0 +1,25 @@
+var yourName = prompt('What is your name?')
+var random = Math.random()
+  if (random < .16) {
+    random = 'Mayhem'
+  }
+  else if (random < .31) {
+  random = 'Venom'
+  }
+  else if (random < .46) {
+    random = 'Immortal'
+  }
+  else if (random < .61) {
+    random = 'Burzum'
+  }
+  else if (random < .76) {
+    random = '1349'
+  }
+  else if (random < .91) {
+    random = 'Emporer'
+  }
+  else {
+    random = 'Darkthrone'
+  }
+
+  alert('Your Black Metal name is: ' + random)
--- rockpaperscissors.js
+++ rockpaperscissors.js
@@ -1,49 +1,49 @@
-var userChoice = prompt("Do you choose rock, paper or scissors?");
-var computerChoice = Math.random();
-if (computerChoice < 0.34) {
-	computerChoice = "rock";
-} else if(computerChoice <= 0.67) {
-	computerChoice = "paper";
-} else {
-	computerChoice = "scissors";
-} alert("Computer: " + computerChoice);
-
-function compare (choice1, choice2) {
-
-if(choice1 === choice2) {
-    alert("The result is a tie!");
-}
-
-else if(choice1 === "rock") {
-
-    if(choice2 === "scissors") {
-        alert("rock wins");
-    }
-    else {
-        alert("paper wins");
-    }
-}
-
-else if(choice1 === "paper") {
-
-    if(choice2 === "rock") {
-        alert("paper wins");
-    }
-    else {
-        alert("scissors wins");
-    }
-}
-
-else if(choice1 === "scissors") {
-
-    if(choice2 === "rock") {
-        alert("rock wins");
-    }
-    else {
-        alert("scissors wins");
-    }
-}
-
-}
-
-compare(userChoice, computerChoice)
+// var userChoice = prompt("Do you choose rock, paper or scissors?");
+// var computerChoice = Math.random();
+// if (computerChoice < 0.34) {
+// 	computerChoice = "rock";
+// } else if(computerChoice <= 0.67) {
+// 	computerChoice = "paper";
+// } else {
+// 	computerChoice = "scissors";
+// } alert("Computer: " + computerChoice);
+//
+// function compare (choice1, choice2) {
+//
+// if(choice1 === choice2) {
+//     alert("The result is a tie!");
+// }
+//
+// else if(choice1 === "rock") {
+//
+//     if(choice2 === "scissors") {
+//         alert("rock wins");
+//     }
+//     else {
+//         alert("paper wins");
+//     }
+// }
+//
+// else if(choice1 === "paper") {
+//
+//     if(choice2 === "rock") {
+//         alert("paper wins");
+//     }
+//     else {
+//         alert("scissors wins");
+//     }
+// }
+//
+// else if(choice1 === "scissors") {
+//
+//     if(choice2 === "rock") {
+//         alert("rock wins");
+//     }
+//     else {
+//         alert("scissors wins");
+//     }
+// }
+//
+// }
+//
+// compare(userChoice, computerChoice)
